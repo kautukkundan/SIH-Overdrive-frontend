618a22e8f6e04dca8e39cb4b448ca8082b59f509
kautukkundan <kautukkundan@gmail.com>
2019-12-14 00:38:26 +0530

    notifications update

--- src/UI/Notifications/components/notification.jsx
+++ src/UI/Notifications/components/notification.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 const Notification = props => {
   return (
     <div className="notification">
+      <div className="icon">
+        <Icon
+          name={`sign-${props.type}`}
+          color={props.type === "in" ? "orange" : "green"}
+          size="big"
+        />
+      </div>
       <div className="info">
         <span className="name">{props.name}</span>
         <span className="email">{props.email}</span>
--- src/UI/Notifications/notifications.jsx
+++ src/UI/Notifications/notifications.jsx
@@ -11,21 +11,25 @@ const Notifications = () => {
           name={"Kautuk Kundan"}
           email={"kautukkundan@gmail.com"}
           message={"has requested to join your team."}
+          type={"in"}
         />
         <Notification
           name={"Gaurav Jain"}
           email={"gaurav.jain@gmail.com"}
           message={"has requested to join your team."}
+          type={"in"}
         />
         <Notification
           name={"Bipin Lala"}
           email={"lala.bipin@gmail.com"}
           message={"wants you to join their team"}
+          type={"out"}
         />
         <Notification
           name={"Gunnika Batra"}
           email={"b.gunnika@gmail.com"}
           message={"wants you to join their team"}
+          type={"out"}
         />
       </div>
     </div>
